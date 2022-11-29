44d54c6a876b9ad27db2c8f9a7b7ed3f7a4d4621
mamonchikmarina <mamonchik.marina@yandex.ru>
2022-11-29 22:44:45 +0300

    hw1

---
+++ задача1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите число: ");
+string numberFirst = Console.ReadLine();
+int number = Convert.ToInt32(numberFirst);
+
+int firstDigit = number / 10;
+
+int secondDigit = firstDigit - (firstDigit / 10) * 10;
+
+Console.WriteLine($"{secondDigit}");
